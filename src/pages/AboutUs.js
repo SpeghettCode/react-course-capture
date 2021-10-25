8f7181c712010512a4678652073783d80b07b2a2
connor <connor.boulton@hotmail.com>
2021-10-25 10:33:02 +0100

    3. Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,15 @@
 import react from "react";
 // import components
 import AboutSection from "../component/AboutSection";
+import ServicesSection from '../component/ServicesSection';
 
 const AboutUs = () => {
     return (
+        <>
             <AboutSection />
-    );
+            <ServicesSection />
+        </>
+    )
 }
 
 export default AboutUs;
