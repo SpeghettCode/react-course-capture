c9ec59847811f1678a8c13a950f54dc2cc7421e8
connor <connor.boulton@hotmail.com>
2021-10-25 11:27:52 +0100

    6. Service Section Styling

--- src/component/AboutSection.js
+++ src/component/AboutSection.js
@@ -2,6 +2,7 @@ import react from "react";
 import home1 from '../img/home1.png';
 // styled
 import styled from 'styled-components';
+import { About, Desc, Img, Hide } from "../styles";
 
 const AboutSection = () => {
     return (
@@ -30,37 +31,5 @@ const AboutSection = () => {
 }
 
 //styled component
-const About = styled.div`
-    min-height: 90vh;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5rem 10rem;
-    color: #fff;
-    `;
-
-const Desc = styled.div`
-    flex: 1;
-    padding-right: 5rem;
-
-    h2 {
-        font-weight: lighter;
-    }
-`;
-
-const Img = styled.div`
-    flex: 1;
-    overflow: hidden;
-
-    img {
-        width: 100%;
-        height: 80vh;
-        object-fit: cover;
-    }
-`;
-
-const Hide = styled.div`
-    overflow: hidden;
-`;
 
 export default AboutSection;
