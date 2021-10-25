9f3e0c269727d97f207d95bc4a3fecb1437ff494
connor <connor.boulton@hotmail.com>
2021-10-25 10:11:56 +0100

    2. About Secion

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import react from "react";
+// import components
+import AboutSection from "../component/AboutSection";
+
+const AboutUs = () => {
+    return (
+            <AboutSection />
+    );
+}
+
+export default AboutUs;
